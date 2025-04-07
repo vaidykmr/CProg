aea65dd90412d1efe95d5e3a543e56e47c99924f
vaidykmr <vaidyanathank98@gmail.com>
2025-04-07 21:09:09 +0000

    code to generate samples of sine wave

--- prog/main.c
+++ prog/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <math.h>
+
 int main()
 {
-    printf("Hello World C");
-    return 0;
+    int duration = 2;  // 2 seconds time duration
+    float samples_per_second = 44100;  // sampling frequency
+    int max_value_encoding = 32767; //max value of short int
+    int tone_frequency = 440; // sine wave frequency = 440 Hz
+
+    int i = 0;  // time step (sample time step (ith sample))
+    int total_samples = duration * samples_per_second; // calculate the total samples for 2 seconds of time duration
+    float time = 0;
+    float angle;
+    short int samples;
+    while(i < total_samples)
+    {
+        time = i/samples_per_second;
+        angle = 2*3.14*tone_frequency*time;
+        samples = max_value_encoding * sin(angle);
+        printf("%d ", samples);
+        i = i+1;
+    }
+
 }
