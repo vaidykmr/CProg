610291215b3973af8212591f4e634a474fc41bfa
vaidykmr <vaidyanathank98@gmail.com>
2025-08-05 13:00:21 +0000

    Find the max avg of the sub array of given length.

--- arrays/sliding_window/main.c
+++ arrays/sliding_window/main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
-void sliding_window(int *arr, int size, int window_length)
+int sliding_window(int *arr, int size, int window_length)
 {
-    int window_sum = 0;
-    int count = 0;
+    float window_sum = 0.00;
+    float previous_window_average = 0.00;
+    float current_window_average = 0.00;
     for (int i = 0; i < window_length; i ++)
     {
         window_sum += arr[i];
     }
-    count = count + 1;
-    printf("Window sum %d : %d", count, window_sum);
+    previous_window_average = window_sum / window_length; 
     /* window_sum = 0;
     for (int i = window_length - 1; i <= ((size - window_length) + 1); i++)
     {
@@ -23,21 +23,26 @@ void sliding_window(int *arr, int size, int window_length)
     for (int i = window_length; i < size; i++)
     {
         window_sum += arr[i] - arr[i - window_length];
-        count ++;
-        printf("\nWindow sum %d : %d ", count, window_sum);
+        current_window_average = window_sum / window_length;
+        
+        if (current_window_average > previous_window_average)
+        {
+            previous_window_average = current_window_average;
+        }
     }
 
-
+    return previous_window_average;
 }
 
 
 int main()
 {
-    int arr[7] = {1, 2, 3, 4, 5, 6, 7};
+    int arr[7] = {3,5,2,6,4,1,7};
     int *arr_ptr = arr;
     int length = sizeof(arr)/sizeof(int);
 
     int k = 3;
 
-    sliding_window(arr_ptr, length, k);
+    float window_avg_max = sliding_window(arr_ptr, length, k);
+    printf("%f ", window_avg_max);
 }
